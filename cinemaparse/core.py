ecd25d69707f027b5c55043f5ee8cda07bc3c90e
ekhossejn <45770055+ekhossejn@users.noreply.github.com>
2019-11-07 21:31:35 +0300

    get_film_cheapest_session

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -83,10 +83,49 @@ class CinemaParser:
             name = cinema.find(class_="underdashed").text.lower()
             if name.find(the_name) != -1 and not cinema_is_found:
                 cinema_is_found = True
-                metro = [elem.replace('м. ', '') for elem in cinema.find(class_="medium-font location").text.split(',')]
+                metros = cinema.find(class_="medium-font location").text.split(',')
+                metro = [elem.replace('м. ', '') for elem in metros]
                 ans += metro
             elif name.find(the_name) != -1 and cinema_is_found:
                 return Exception('More than 1 cinema has got this name')
         if ans:
             return ans
         return Exception('There is no cinema with this name')
+
+    def get_film_cheapest_session(self, film):
+        """Возвращение самого дешевого сеанса, кинотеатра и времени"""
+        self.extract_raw_content()
+        self.get_films_list()
+        page = requests.get('https://{}.subscity.ru/'.format(self.town))
+        page = BeautifulSoup(page.text, 'html.parser')
+        for name in page.find_all(class_="movie-plate"):
+            if name.get('attr-title') == film:
+                url = 'https://{}.subscity.ru/'.format(self.town)\
+                                                                  + name.find('a').get('href')
+                page = requests.get(url)
+                page = BeautifulSoup(page.text, 'html.parser')
+                day = page.find(class_='header-day text-center')
+                day = day.find(class_='underdashed').get('href')
+                if day != '/dates/' + datetime.today().strftime('%Y-%m-%d'):
+                    return None, None, None
+                name = 'table table-bordered '
+                name += 'table-condensed table-curved '
+                name += 'table-striped table-no-inside-borders'
+                timetable = page.find(class_=name).find_all(class_='row-entity')
+                price_info = dict()
+                for session in timetable:
+                    cinema = session.find(class_='underdashed').text
+                    nums = session.find_all(class_='text-center cell-screenings')
+                    for num in nums:
+                        price = num.get('attr-price')
+                        name = 'btn btn-default navbar-btn price-button cell-screening-desktop'
+                        time_1 = num.find(class_=name).text
+                        if price is None:
+                            price = 1e9
+                        else:
+                            price = int(price)
+                        if datetime.now().strftime('%H:%M') < time_1:
+                            price_info[price] = (cinema, time_1)
+                price_info = sorted(price_info.items(), key=lambda x: x[0])
+                return price_info[0][1][0], price_info[0][1][1], price_info[0][0]
+        return None, None, None
